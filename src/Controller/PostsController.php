8b4cb5b88dcb5f4211d686af5da87a702de5a7ab
Damien <aubry_damien@orange.fr>
2022-06-10 16:35:44 +0200

    Add show() method and its template

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,8 +23,16 @@ class PostsController extends AbstractController
          */
         $posts = $postRepository->findAll();
 
-        return $this->render('posts/index.html.twig', [
-            'posts' => $posts
-        ]);
+        return $this->render('posts/index.html.twig', compact('posts'));
+    }
+
+    /**
+     * @param Post $post
+     * @return Response
+     */
+    #[Route('/posts/{slug}', name: 'app_posts_show')]
+    public function show(Post $post): Response
+    {
+        return $this->render('posts/show.html.twig', compact('post'));
     }
 }

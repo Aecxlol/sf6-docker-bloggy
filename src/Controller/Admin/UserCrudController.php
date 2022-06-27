aa5d6ea9668b717a4b153c64da25dbcc01f3130a
Damien <aubry_damien@orange.fr>
2022-06-28 01:54:50 +0200

    Make use of ChoiceField (easyadmin)

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -28,7 +28,11 @@ class UserCrudController extends AbstractCrudController
                 ->setRequired($pageName === Crud::PAGE_NEW)
                 ->onlyOnForms()
                 ->setFormType(PasswordType::class),
-            ArrayField::new('roles'),
+            ChoiceField::new('roles')
+                ->setChoices(['Admin' => 'ROLE_ADMIN', 'User' => 'ROLE_USER'])
+                ->allowMultipleChoices()
+                ->renderExpanded()
+                ->renderAsBadges(),
         ];
     }
 }
